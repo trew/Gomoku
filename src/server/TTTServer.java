4eb785af0bf46f7f814160205c24088afdafe8a9
Samuel Andersson <smulster@gmail.com>
2012-12-30 23:44:08 +0100

    Improve the server to use a swing window as a console

--- src/server/TTTServer.java
+++ src/server/TTTServer.java
@@ -1,6 +1,16 @@
 package server;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
+import java.io.PrintStream;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 import tictactoe.Board;
 
@@ -9,16 +19,23 @@ import net.*;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Server;
+import com.esotericsoftware.minlog.Log;
+import static com.esotericsoftware.minlog.Log.*;
 
 /**
- * Server of the Tic-tac-toe game
+ * Server of the Tic-tac-toe game<br />
+ * <br />
+ * Possible arguments<br />
+ * <b>--port</b> <i>PORT</i> - The port number which we'll run the server on<br />
+ * <b>--swing</b> - Whether we should run with swing or use standard console.
+ * (Swing is always used on windows)<br />
  *
  * @author Samuel Andersson
  */
 public class TTTServer {
 
 	/** The port which this server is listening on */
-	private static final int PORT = 9123;
+	private static int PORT = 9123;
 
 	private Server server;
 	private ServerListener listener;
@@ -50,6 +67,10 @@ public class TTTServer {
 		}
 	}
 
+	public void exit() {
+		server.stop();
+	}
+
 	public void broadcast(Connection conn, Object obj) {
 		server.sendToAllExceptTCP(conn.getID(), obj);
 	}
@@ -59,11 +80,55 @@ public class TTTServer {
 	 *
 	 * @param args
 	 *            Any arguments passed to the server
+	 * @throws IOException
 	 */
-	public static void main(String[] args) {
-		TTTServer srv = new TTTServer();
-		srv.init();
-		srv.start();
+	public static void main(String[] args) throws IOException {
+		Log.set(LEVEL_DEBUG);
+
+		boolean useSwing = false;
+		if (System.getProperty("os.name").startsWith("Windows")) {
+			useSwing = true;
+		}
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals("--port") && i != args.length) {
+				i++;
+				PORT = Integer.parseInt(args[i]);
+			} else if (args[i].equals("--swing")) {
+				useSwing = true;
+			}
+		}
+
+		final TTTServer server;
+		if (useSwing) {
+			JFrame frame = new JFrame();
+			frame.add(new JLabel(" Output "), BorderLayout.NORTH);
+
+			JTextArea ta = new JTextArea();
+			Console co = new Console(ta);
+			PrintStream ps = new PrintStream(co);
+			System.setOut(ps);
+			System.setErr(ps);
+
+			frame.add(new JScrollPane(ta));
+
+			frame.setMinimumSize(new Dimension(350, 300));
+			server = new TTTServer();
+
+			frame.addWindowListener(new WindowAdapter() {
+				@Override
+				public void windowClosing(WindowEvent e) {
+					server.exit();
+					System.exit(0);
+				}
+			});
+			frame.pack();
+			frame.setVisible(true);
+		} else {
+			server = new TTTServer();
+		}
+
+		server.init();
+		server.start();
 	}
 
 }
